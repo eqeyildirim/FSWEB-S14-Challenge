9229502845252aec3d0895e04b9d1d2321280fe2
eqeyildirim <eqeyildirims@gmail.com>
2024-12-04 23:10:23 -0300

    miss comp

---
+++ src/main/java/org/example/models/Hamburger.java
@@ -0,0 +1,67 @@
+package org.example.models;
+
+public class Hamburger {
+    private String name;
+    private String meat;
+    private double price;
+    private String breadRollType;
+
+    private String addition1Name;
+    private double addition1Price;
+
+    private String addition2Name;
+    private double addition2Price;
+
+    private String addition3Name;
+    private double addition3Price;
+
+    private String addition4Name;
+    private double addition4Price;
+
+    public Hamburger(String name, String meat, double price, String breadRollType) {
+        this.name = name;
+        this.meat = meat;
+        this.price = price;
+        this.breadRollType = breadRollType;
+    }
+
+    public void addHamburgerAddition1(String name, double price){
+        this.addition1Name=name;
+        this.addition1Price=price;
+    }
+    public void addHamburgerAddition2(String name, double price){
+        this.addition2Name=name;
+        this.addition2Price=price;
+    }
+    public void addHamburgerAddition3(String name, double price){
+        this.addition3Name=name;
+        this.addition3Price=price;
+    }
+    public void addHamburgerAddition4(String name, double price){
+        this.addition4Name=name;
+        this.addition4Price=price;
+    }
+
+    public double itemizeHamburger() {
+        double totalPrice = this.price;
+        if (this.addition1Name != null) totalPrice += this.addition1Price;
+        if (this.addition2Name != null) totalPrice += this.addition2Price;
+        if (this.addition3Name != null) totalPrice += this.addition3Price;
+        if (this.addition4Name != null) totalPrice += this.addition4Price;
+        this.price = totalPrice;
+        return totalPrice;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getMeat() {
+        return meat;
+    }
+    public double getPrice() {
+        return this.price;
+    }
+    public String getBreadRollType() {
+        return breadRollType;
+    }
+}
